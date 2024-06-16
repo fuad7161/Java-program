d08a559377d0df8e3e28e87726cb708bad9bade9
fuad7161 <fuadul202@gmail.com>
2024-06-16 15:07:19 +0600

    Variables added

---
+++ JavaBasics/Variables/Variables.java
@@ -0,0 +1,17 @@
+/*
+ * Three types of variabes are there
+ * Local variable , Instance variable , static bariable
+    byte (1 byte)
+    short (2 bytes)
+    int (4 bytes)
+    long (8 bytes)
+
+ */
+public class Variables {
+    int data = 99; // instance variable(outside of the method and without any static keyword)
+    static int a = 1; // static variable
+
+    public static void main(String[] args) {
+        int a = 10; // local variabls
+    }
+}
